a6897a602872154a98d69add66c5149bad7c581d
prakashhchaudharyy <00prakashchaudhary@gmail.com>
2025-06-27 10:31:29 +0545

    edited LoginModule

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
-import Login from '../components/auth/login'
+//import LoginModule from '../components/auth/login'
 import LoadingSpinner from "../components/ui/loadingSpinner";
 
 export default function Home() {
   return (
+    //<LoginModule/>
     <LoadingSpinner/>
+  
   );
 }
